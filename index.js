2b43530dbc6cb1df3048dd50dd3a5b3f192a58b9
Matt Christopherson <153093689+Matt-Christopherson@users.noreply.github.com>
2024-03-05 11:36:09 -0600

    Update index.js
    Removed To Do comments

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
-// TODO: Create an array of questions for user input
+
 const questions = [
     {
         type: 'input',
@@ -63,12 +62,10 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((responses) => {
         responseTitle = (responses.title).replace(/\s+/g, '');
