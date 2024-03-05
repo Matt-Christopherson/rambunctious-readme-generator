0f4bb14e917019b57f9c437cc32e236e24324a67
Matt Christopherson <153093689+Matt-Christopherson@users.noreply.github.com>
2024-03-05 11:35:18 -0600

    Update generateMarkdown.js
    Removed comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
@@ -17,17 +15,6 @@ function renderLicenseBadge(license) {
   return '';
 }
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//   if (license !== "none") {
-//     return `https://choosealicense.com/licenses/${license}/`
-//   }
-//   return '';
-// }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'none') {
     return `## License ${renderLicenseBadge(license)}
@@ -37,7 +24,6 @@ function renderLicenseSection(license) {
   return '';
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 ## Description
